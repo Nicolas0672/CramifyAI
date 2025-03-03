b87ab97053c7a9fe34af81b242e9673ab1e21770
Nicolas0672 <140045078+Nicolas0672@users.noreply.github.com>
2025-03-02 23:17:35 -0800

    Installed AUTH and Started Building on DashBoard

---
+++ app/dashboard/_components/WelcomeBanner.jsx
@@ -0,0 +1,19 @@
+"use client"
+import { useUser } from '@clerk/nextjs'
+import Image from 'next/image'
+import React from 'react'
+
+function WelcomeBanner() {
+    const {user} = useUser()
+  return (
+    <div className='flex items-center gap-6 p-5 bg-gray-600 w-full text-white rounded-lg'>
+        <Image src={'/laptop.png'} alt='laptop' width={100} height={100}/>
+        <div>
+            <h2 className='font-bold text-3xl'>Hello, {user?.fullName}</h2>
+            <p>Welcome Back, it is time to lock in</p>
+        </div>
+    </div>
+  )
+}
+
+export default WelcomeBanner
